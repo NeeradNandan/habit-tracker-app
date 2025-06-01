f493620d7350c9ff1aafe5f48696748139915a93
Neerad Nandan <nrdnandan1705@gmail.com>
2025-06-01 21:54:31 +0530

    chore(app): Basic folder structure and logic - Implement logic to _layout.tsx files under app and (tabs) folder - Implement basic logic for now to index.tsx and apply nativewind CSS to it - Add dummy Login page/route for Tab view

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,28 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+/*const styles = StyleSheet.create(
+    {
+        view: {
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        navButton: {
+            width: 100,
+            height: 30,
+            backgroundColor: "coral",
+            textAlign: "center",
+            borderRadius: 8
+        }
+    }
+)
+
+ */
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,28 @@
 import '../global.css';
+import { Stack, usePathname, useRouter } from "expo-router";
+import { useEffect } from "react";
 
-import { Stack } from 'expo-router';
+function RouteGuard({ children }: { children: React.ReactNode }) {
+  const isAuth = false;
+  const router = useRouter();
+  const pathname = usePathname();
+  useEffect(() => {
+    if(!isAuth && pathname !== "/Auth") {
+      //router.replace("/Auth");
+    }
+  });
+  
+  return <>{children}</>;
+}
 
-export default function Layout() {
-  return <Stack />;
+export default function RootLayout() {
+  return (
+      <Stack>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="Auth" />
+        </Stack>
+      </Stack>
+  );
 }
